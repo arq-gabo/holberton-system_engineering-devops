c7bbfb46997d28fad33f26e8b34a68cd8f6ad6e5
arq-gabo <jgabrielguerral>
2019-12-11 05:14:27 +0000

    [FIX]Correction_puppet_lint

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 #File of configuration with puppet
 exec { 'line':
-command =>'/bin/echo -e "PasswordAuthentication no IdentityFile ~/.ssh/holberton" >> ~/.ssh/ssh_config',
+command =>'/bin/echo -e "PasswordAuthentication no\n IdentityFile ~/.ssh/holberton" >> ~/.ssh/ssh_config',
 path    =>'/usr/bin'
 }
