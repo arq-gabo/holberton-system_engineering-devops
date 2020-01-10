71dacf70b836dbac8e54f585a0ac56aa02e646f9
arq-gabo <jgabrielguerral@gmail.com>
2020-01-09 19:49:33 -0500

    [FIX]Task_02

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,20 +1,30 @@
 #Script in Puppet the task creating a custon HTTP header
-exec {'update_server':
-command => '/usr/bin/apt-get update',
+exec 
+{
+'update_server': command => '/usr/bin/apt-get update',
 }
--> package {'nginx':
-ensure => 'present',
+
+-> package
+{
+'nginx': ensure => 'present',
 }
--> file {'/etc/nginx/sites-available/default':
-ensure => 'present',
+
+-> file
+{
+'/etc/nginx/sites-available/default': ensure => 'present',
 }
--> file_line { 'check a line':
+
+-> file_line
+{
+'check a line':
 ensure => 'present',
 path   => '/etc/nginx/sites-available/default',
 line   => "   location / {
              add_header X-Served-By ${hostname};",
 match  => '^\tlocation / {',
 }
--> exec { 'start/restart server':
-command => '/usr/sbin/service nginx restart',
+
+-> exec 
+{
+'start/restart server': command => '/usr/sbin/service nginx restart',
 }
