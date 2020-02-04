d4bbcb096cd471de07a81faa5781b46fd14f9410
arq-gabo <jgabrielguerral@gmail.com>
2020-02-04 05:03:56 +0000

    [FIX]task_02

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""Module for export to in CSV file"""
+
+
+import requests
+import sys
+import json
+
+if __name__ == '__main__':
+
+    url = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
+    res = url.json()
+
+    todo_list = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.
+        format(sys.argv[1]))
+    res2 = todo_list.json()
+
+    with open('{}.json'.format(sys.argv[1]), 'w') as r:
+
+        data = {}
+        id_usr = res.get('id')
+        data[id_usr] = []
+
+        for i in res2:
+            data[id_usr].append({
+                "task": i.get("title"),
+                "completed": i.get("completed"),
+                "username": res.get("username")})
+
+        json.dump(data, r)
