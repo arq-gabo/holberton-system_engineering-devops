d05c3dea84b1215ef5b8450a1603cd987518ea74
arq-gabo <jgabrielguerral@gmail.com>
2020-02-04 04:27:22 +0000

    [FIX]Task_00

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,12 +16,20 @@ if __name__ == '__main__':
     res = url.json()
 
     todo_list = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(sys.argv[1]))
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.
+        format(sys.argv[1]))
     res2 = todo_list.json()
-    print(res2)
 
+    todo_len = len(res2)
 
-"""
+    complete_true = 0
+    for todo in res2:
+        if todo.get('completed'):
+            complete_true += 1
+
     name_employe = res['name']
-    print("Employee {} is done with:".format(name_employe)
-"""
+    print("Employee {} is done with({}/{}):".format(
+        name_employe, complete_true, todo_len))
+    for str_title in res2:
+        if str_title.get('completed'):
+            print('\t {}'.format(str_title['title']))
