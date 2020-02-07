d748cef39016ade21392cc1b892a97b3de6ac3e2
arq-gabo <jgabrielguerral@gmail.com>
2020-02-06 23:34:10 -0500

    [FIX]Task_00

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
 """
-Module REST API for return number of subcriptons of reddit
+Module API for return number of subcriptons of reddit
 """
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
     """Function for return the number of number of subscribers"""
-    headers = {'User_Agent': 'arq-gabo'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent":  "arq-gabo"}
+    r = requests.get(url, headers=headers)
     try:
-        reddit = get('https://www.reddit.com/r/{}/about.json'.format(
-            subreddit), headers=headers)
-        sub = reddit.get('data').get('subscribers')
-        return(sub)
-    except:
+        subs = r.json().get("data").get("subscribers")
+        subscribers = subs if subs is not None else 0
+        return(subscribers)
+    except AttributeError:
         return(0)
