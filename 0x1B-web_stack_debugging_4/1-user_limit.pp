cadf4f8b9be710b9cd53dc3dfdd96fa3b1b019d5
arq-gabo <jgabrielguerral@gmail.com>
2020-02-26 04:15:43 +0000

    [FEATURE]Task_01

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+exec { 'fix limits.conf hard':
+  command => 'sed -i "s/hard nofile 5/rd nofile 1024/g" limits.conf',
+  path    => '/bin/',
+  cwd     => '/etc/security/',
+}
+exec { 'fix limits.conf soft':
+  command => 'sed -i "s/soft nofile 4/ft nofile 1023/g" limits.conf',
+  path    => '/bin/',
+  cwd     => '/etc/security/',
+}
